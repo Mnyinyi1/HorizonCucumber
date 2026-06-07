52c981eaa80f86d8c1b4d465cc3be95d1c171086
mnyinyi <mnyinyi20@gmail.com>
2026-06-07 19:28:28 +0200

    Added extentreports files and test runner

---
+++ src/test/java/TestRunners/TestRunner.java
@@ -0,0 +1,18 @@
+package TestRunners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(
+        features = "src/test/resources/Features",
+        glue = {"StepDefs"},
+        plugin =
+                {"pretty", "html:target/cucumber-reports.html", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        monochrome = true
+        //tags = "@Generateinvoice"
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+}
+
