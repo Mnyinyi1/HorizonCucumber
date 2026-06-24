5ba08c5475fdf09c0cc0d4e2ce05d82d3ef871bc
mnyinyi <mnyinyi20@gmail.com>
2026-06-24 13:02:57 +0200

    Added step definitions

--- src/test/java/PageObjects/HomePage.java
+++ src/test/java/PageObjects/HomePage.java
@@ -1,25 +1,56 @@
 package PageObjects;
 
-import Utils.TakeScreenshots;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static java.lang.Thread.sleep;
+import java.time.Duration;
+
 
 public class HomePage {
     WebDriver driver;
 
     public HomePage(WebDriver driver) {
+
         this.driver = driver;
     }
 
-    @FindBy(xpath = "//a[.//span[text()='Client Capture']]")
-    WebElement clientcapture_xpath;
+    @FindBy(xpath = "//li[@class='rz-navigation-item']//span[text()='Home']")
+    WebElement HomePageLink_xpath;
 
-    public void verifyInformationBoxIsDisplayed() throws InterruptedException {
-          Thread.sleep(5000);
-        clientcapture_xpath.isDisplayed();
-        TakeScreenshots.takeScreenshot(driver, "HomePage");
+    @FindBy(xpath = "//span[text()='Client Capture']")
+    WebElement ClientCapture_xpath;
+
+    public void ClickClientCapture() {
+        new WebDriverWait(driver, Duration.ofSeconds(50))
+                .until(ExpectedConditions.elementToBeClickable(ClientCapture_xpath));
+        ClientCapture_xpath.click();
+
     }
+    @FindBy(xpath = "//button[contains(@class,'rz-button')]//span[text()='New Quote Capture']")
+    WebElement NewQuoteCaptureButton_xpath;
+
+    public void ClickNewQuoteCaptureButton() {
+        new WebDriverWait(driver, Duration.ofSeconds(20))
+                .until(ExpectedConditions.elementToBeClickable(NewQuoteCaptureButton_xpath));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", NewQuoteCaptureButton_xpath);
+        try {
+            NewQuoteCaptureButton_xpath.click();
+        } catch (Exception e) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", NewQuoteCaptureButton_xpath);
+        }
+    }
+    @FindBy(xpath = "//div[contains(@class,'rz-dropdown')]//span[contains(@class,'rz-dropdown-label')]")
+    WebElement SelectProduct_xpath;
+
+    public void ClickSelectProduct() {
+        new WebDriverWait(driver, Duration.ofSeconds(20))
+                .until(ExpectedConditions.elementToBeClickable(SelectProduct_xpath));
+        SelectProduct_xpath.click();
+    }
+
+
 }
--- target/test-classes/features/Horizon.feature
+++ target/test-classes/features/Horizon.feature
@@ -1,5 +1,6 @@
 Feature: Horizon
 
+  @login
   Scenario Outline: As Clientele use I want to be able to login on platform
     Given the user  has navigated to the login page
     When the user enters username "<username>"
@@ -7,10 +8,29 @@ Feature: Horizon
     And clicks on the login button
     And user should see error message "<errormessage>"
 
-
 Examples:
     |username  |  password|errormessage|
-    | mnyinyi@clientele.co.za|10Collections123$|                            |
     | mnyinyi@clientele.co.za|10Coections123$ |Invalid username or password.|
+    | mnyinyi@clientele.co.za|10Collections123$|                            |
+
+@Quote
+Scenario Outline: As User I want to be able to capture Quote
+  Given  In home Page user clicks on capture Quote'<username>','<password>'
+  When Clicks on  new capture Quote
+  And Capture product details and  click continue button '<Product>','<Plan>'
+  And Capture client details and Plan detail '<SaleChannel>','<PolicyType>','<LegalRefeenceType>','<LegalReferenceNumber>','<Title>','<FirstName>','<LastName>','<Citzenship>','<Gender>','<Smoker>','<CellNo>','<Address1>','<Suburb>','<CityTown>'
+  And Clicks on review benefits button and Click continue button
+  And Clicks on save button
+  And user Navigate to Payer
+  And Capture payer information and Account information '<Relationshiptomember>','<Paymentmethod>','<AccountHolder>','<Debitday>','<BankName>','<BranchCode>','<AccountNumber>','<AccountType>'
+  And Click save button
+
+  Examples:
+    |username|password|Product|Plan|SaleChannel|PolicyType|LegalRefeenceType|LegalReferenceNumber|Title|FirstName|LastName|Citzenship|Gender|Smoker|CellNo|Address1|Suburb|CityTown|Relationshiptomember|Paymentmethod|AccountHolder|Debitday|BankName|BranchCode|AccountNumber|AccountType|
+    | mnyinyi@clientele.co.za|10Collections123$|Clientele Funeral Dignity Plan|Clientele Funeral Dignity Plan|Broker|Member Only|ID Number|8901015009087|Mr|Nala|Nyinyi|South African|Male|Non-Smoker|0821234567|123 Main Street|Suburbia|City ville|Member|Debit Order|Maili Bank|1|First National Bank|250655|1234|Savings|
+
+
+
+
 
 
